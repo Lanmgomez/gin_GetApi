a27bf835e4ed6470a11f8ba3841863a28017a795
Islan Gomes da Silva <islan.gomes@supplylabs.com.br>
2024-07-13 13:46:57 -0300

    Melhorando código

--- main.go
+++ main.go
@@ -4,27 +4,46 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type USERS struct {
+	ID       float64
+	Name     string
+	Email    string
+	CreatAt  string
+	UpdateAt string
+}
+
 func userData(c *gin.Context) {
-	users := []map[string]interface{}{
+
+	users := []USERS{
 		{
-			"name":     "Islan",
-			"email":    "islan_gomes@hotmail.com",
-			"creatAt":  "10/08/2024",
-			"updateAt": "10/08/2024",
+			Name:     "Islan",
+			Email:    "islan_gomes@hotmail.com",
+			CreatAt:  "10/08/2024",
+			UpdateAt: "10/08/2024",
 		},
 		{
-			"name":     "Vinicius",
-			"email":    "vinicius_mocci@hotmail.com",
-			"creatAt":  "11/08/2024",
-			"updateAt": "11/08/2024",
+			Name:     "Vinicius",
+			Email:    "vinicius_mocci@hotmail.com",
+			CreatAt:  "10/08/2024",
+			UpdateAt: "10/08/2024",
 		},
 	}
+
+	finalUsers := []USERS{}
 
 	for index, user := range users {
-		user["id"] = index + 1
+		var currentUser USERS
+
+		currentUser.ID = float64(index) + 1
+		currentUser.Name = user.Name
+		currentUser.Email = user.Email
+		currentUser.CreatAt = user.CreatAt
+		currentUser.UpdateAt = user.UpdateAt
+
+		finalUsers = append(finalUsers, currentUser)
 	}
 
-	c.JSON(200, users)
+	c.JSON(200, finalUsers)
 }
 
 func main() {
