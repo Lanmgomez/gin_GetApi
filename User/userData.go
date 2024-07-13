433ae30da866cf610c777bd077a361016f86bcc9
Islan Gomes da Silva <islan.gomes@supplylabs.com.br>
2024-07-13 19:48:28 -0300

    Melhorando código de novo

---
+++ User/userData.go
@@ -0,0 +1,33 @@
+package User
+
+type USERS struct {
+	ID       float64
+	Name     string
+	Email    string
+	CreatAt  string
+	UpdateAt string
+}
+
+func UsersData() []USERS {
+
+	users := []USERS{
+		{Name: "Islan", Email: "islan@teste.com", CreatAt: "26/05/1997", UpdateAt: "10/08/2024"},
+		{Name: "Vinicius", Email: "vini@teste.com", CreatAt: "21/05/1997", UpdateAt: "10/08/2024"},
+	}
+
+	finalUsers := []USERS{}
+
+	for index, user := range users {
+		var currentUser USERS
+
+		currentUser.ID = float64(index) + 1
+		currentUser.Name = user.Name
+		currentUser.Email = user.Email
+		currentUser.CreatAt = user.CreatAt
+		currentUser.UpdateAt = user.UpdateAt
+
+		finalUsers = append(finalUsers, currentUser)
+	}
+
+	return finalUsers
+}
--- main.go
+++ main.go
@@ -1,47 +1,13 @@
 package main
 
 import (
+	"github.com/Lanmgomez/go-gin-api/User"
 	"github.com/gin-gonic/gin"
 )
 
-type USERS struct {
-	ID       float64
-	Name     string
-	Email    string
-	CreatAt  string
-	UpdateAt string
-}
-
 func userData(c *gin.Context) {
 
-	users := []USERS{
-		{
-			Name:     "Islan",
-			Email:    "islan_gomes@hotmail.com",
-			CreatAt:  "10/08/2024",
-			UpdateAt: "10/08/2024",
-		},
-		{
-			Name:     "Vinicius",
-			Email:    "vinicius_mocci@hotmail.com",
-			CreatAt:  "10/08/2024",
-			UpdateAt: "10/08/2024",
-		},
-	}
-
-	finalUsers := []USERS{}
-
-	for index, user := range users {
-		var currentUser USERS
-
-		currentUser.ID = float64(index) + 1
-		currentUser.Name = user.Name
-		currentUser.Email = user.Email
-		currentUser.CreatAt = user.CreatAt
-		currentUser.UpdateAt = user.UpdateAt
-
-		finalUsers = append(finalUsers, currentUser)
-	}
+	finalUsers := User.UsersData()
 
 	c.JSON(200, finalUsers)
 }
